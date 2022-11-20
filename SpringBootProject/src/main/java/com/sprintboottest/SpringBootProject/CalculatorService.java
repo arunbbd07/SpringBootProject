e147a097d5202deeb212662f9b4eed6d43844674
Arun Kumar Yadav <37401773+arunbbd07@users.noreply.github.com>
2022-11-20 15:39:40 +0530

    Add files via upload
    Calculator API

---
+++ SpringBootProject/src/main/java/com/sprintboottest/SpringBootProject/CalculatorService.java
@@ -0,0 +1,34 @@
+package com.sprintboottest.SpringBootProject;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculatorService {
+
+	@GetMapping("/add/{x}/{y}")
+	public Response addition(@PathVariable int x, @PathVariable int y) {
+
+		return new Response(x, y, x + y);
+	}
+
+	@GetMapping("/sub/{x}/{y}")
+	public Response substraction(@PathVariable int x, @PathVariable int y) {
+
+		return new Response(x, y, x - y);
+	}
+
+	@GetMapping("/mul/{x}/{y}")
+	public Response multiplication(@PathVariable int x, @PathVariable int y) {
+
+		return new Response(x, y, x * y);
+	}
+
+	@GetMapping("/div/{x}/{y}")
+	public Response division(@PathVariable int x, @PathVariable int y) {
+
+		return new Response(x, y, x / y);
+	}
+
+}
---
+++ SpringBootProject/src/test/java/com/sprintboottest/SpringBootProject/SpringBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprintboottest.SpringBootProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
